7c92c1f138f0c41ed94fef03ca3ae30c2251936d
Prerak143-stack <61062101+Prerak143-stack@users.noreply.github.com>
2020-02-24 21:47:31 -0500

    Add files via upload

---
+++ counterSum.py
@@ -0,0 +1,12 @@
+# I am Prerak Patel and my student number is 000736376, I have designed this
+# program by myself and I have not shared this anyone.My professor's name is
+# TIfany,Antopoloski
+
+x = int(input("Enter any number of your choice :"))
+
+total = 0
+counter = 0
+while (counter < x):
+    counter += 1
+    total = total + (x - counter)
+print(total)
