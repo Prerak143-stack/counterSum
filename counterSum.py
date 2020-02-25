599a94744ee7f3d1a4d70d642316b6f114d494fe
developedbypatels <61062101+developed-bypatels@users.noreply.github.com>
2020-02-25 02:15:52 -0500

    Update counterSum.py

--- counterSum.py
+++ counterSum.py
@@ -1,6 +1,4 @@
-# I am Prerak Patel and my student number is 000736376, I have designed this
-# program by myself and I have not shared this anyone.My professor's name is
-# TIfany,Antopoloski
+
 
 x = int(input("Enter any number of your choice :"))
 
